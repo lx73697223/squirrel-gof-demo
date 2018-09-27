3e8c734ac9e658e212e124e9d87ac600ada5e41f
LiXiang <lixiang01234567890@163com@163.com>
2018-09-27 12:56:53 +0800

    null object pattern

---
+++ src/main/java/com/pipi/gof/nullobject/NullCustomer.java
@@ -0,0 +1,15 @@
+package com.pipi.gof.nullobject;
+
+public class NullCustomer extends AbstractCustomer {
+
+    @Override
+    public String getName() {
+        return "Not Available in Customer Database";
+    }
+
+    @Override
+    public boolean isNil() {
+        return true;
+    }
+
+}
---
+++ src/main/java/com/pipi/gof/nullobject/RealCustomer.java
@@ -0,0 +1,19 @@
+package com.pipi.gof.nullobject;
+
+public class RealCustomer extends AbstractCustomer {
+
+    public RealCustomer(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean isNil() {
+        return false;
+    }
+
+}
